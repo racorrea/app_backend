906b9d7cb61451697e6a1fa15804f74cafcf7d36
Roddy Correa <ra.correa90@gmail.com>
2020-06-17 23:41:41 -0500

    se agrega actualizar y eliminar para validar

--- src/main/java/com/racoservices/controller/PersonaController.java
+++ src/main/java/com/racoservices/controller/PersonaController.java
@@ -7,8 +7,11 @@ import com.racoservices.service.IPersonaService;
 
 import org.hibernate.annotations.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +23,6 @@ public class PersonaController {
     @Autowired
     IPersonaService service;
 
-    
-
     @GetMapping
     public List<Persona> listar (){
         return service.listar();
@@ -32,5 +33,19 @@ public class PersonaController {
         return service.save(persona);
     }
 
+    @PutMapping("/{id}")
+    public Persona actualizar(@RequestBody Persona persona, @PathVariable Integer id){
+        Persona npersona = service.findById(id);
+
+        npersona.setNombres(persona.getNombres());
+        npersona.setApellidos(persona.getApellidos());
+
+        return service.actualizar(npersona);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Integer id){
+        return service.delete(id);
+    }
 
 }
